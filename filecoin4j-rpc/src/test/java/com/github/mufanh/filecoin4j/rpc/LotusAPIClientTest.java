7817357e5d238ad95183c5a8f57a2e60af9434c8
xqhuang <xqhuang@chinaums.com>
2021-01-23 22:48:43 +0800

    lotus chainhead async api 还有bug

--- filecoin4j-rpc/src/test/java/com/github/mufanh/filecoin4j/rpc/LotusAPIClientTest.java
+++ filecoin4j-rpc/src/test/java/com/github/mufanh/filecoin4j/rpc/LotusAPIClientTest.java
@@ -1,16 +1,19 @@
 package com.github.mufanh.filecoin4j.rpc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.mufanh.filecoin4j.domain.TipSet;
 import com.github.mufanh.jsonrpc4j.Call;
+import com.github.mufanh.jsonrpc4j.Callback;
 import com.github.mufanh.jsonrpc4j.Response;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 
 import static org.junit.Assert.*;
 
@@ -34,5 +37,24 @@ public class LotusAPIClientTest {
         Response<TipSet> response = lotusAPI.chainHead().execute();
         TipSet result = response.getResult();
         System.out.println(mapper.writeValueAsString(result));
+    }
+
+    @Test
+    public void asyncChainHead() throws IOException {
+        lotusAPI.chainHead().enqueue(new Callback<TipSet>() {
+            @Override
+            public void onResponse(Call<TipSet> call, Response<TipSet> response) {
+                try {
+                    System.out.println(mapper.writeValueAsString(response.getResult()));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TipSet> call, Throwable t) {
+                t.printStackTrace(System.err);
+            }
+        });
     }
 }
